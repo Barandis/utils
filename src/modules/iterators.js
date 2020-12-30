341f71d8027546993d1cf2a87bf031b12611e7e4
Thomas Otterson <barandis1729@gmail.com>
2020-12-30 12:35:17 -0500

    test(iterators): add tests for iterator functions

--- src/modules/iterators.js
+++ src/modules/iterators.js
@@ -27,7 +27,7 @@
 export function *enumerate(iterable, start = 0) {
   const iterator = iterable[Symbol.iterator]()
   let result = iterator.next()
-  let index = start >= 0 ? Math.floor(start) : 0
+  let index = Math.trunc(start)
 
   while (!result.done) {
     yield [index++, result.value]
@@ -40,7 +40,7 @@ export function *enumerate(iterable, start = 0) {
  *
  * This generator yields a range of numbers, starting with `start` and
  * incrementing each succeeding number by `step` until `end` is reached.
- * If `inclusive` is true, then `end` itself will be included in the
+ * If `inclusive` is true, then `end` itself can be included in the
  * range.
  *
  * If only one numeric parameter is provided, it is assumed to be `end`.
@@ -49,9 +49,10 @@ export function *enumerate(iterable, start = 0) {
  *
  * If `start` is greater than `end`, the range will run in reverse. In
  * this case, each successive number is *de*cremented by `step`. `step`
- * itself should always be non-negative; if a negative `step` is
- * provided, its absolute value will be used instead. The direction of
- * the iteration is always determined by `start` and `end`.
+ * itself should always be positive; if a negative `step` is provided,
+ * its absolute value will be used instead, and if `0` is provided, it
+ * will be regarded as `1`. The direction of the iteration is always
+ * determined by `start` and `end`.
  *
  * ### Examples
  *
@@ -69,11 +70,11 @@ export function *enumerate(iterable, start = 0) {
  *
  * // 0 is necessary because range(4, 2) would be interpreted as a
  * // start of 4 and end of 2, producing [4,3]
- * const set = [...range(0, 4, 2)]
- * console.log(set)  // [0,2]
+ * const sep = [...range(0, 4, 2)]
+ * console.log(sep)  // [0,2]
  *
- * const seti = [...range(0, 4, 2, true)]
- * console.log(seti) // [0,2,4]
+ * const sepi = [...range(0, 4, 2, true)]
+ * console.log(sepi) // [0,2,4]
  *
  * // Probably the most common use: in `for...of` loops
  * const a = [0, 1, 2, 3]
@@ -110,26 +111,25 @@ export function *range(start, end, step, inclusive) {
   const e = typeof end === 'number' ? end : start
   const p = typeof step === 'number' ? step === 0 ? 1 : Math.abs(step) : 1
   const i = typeof step === 'number'
-    ? !!inclusive : typeof end === 'number'
-      ? !!step : !!end
+    ? !!inclusive
+    : typeof end === 'number' ? !!step : !!end
 
-  const forward = s < e
-  let current = s
+  const fwd = s < e
+  let cur = s
 
-  const finished = () => {
-    if (forward) {
-      return i ? current > e : current >= e
-    }
-    return i ? current < e : current <= e
-  }
+  // If the range is moving forward, it's finished if the current value
+  // is greater than or equal to the end point (just greater than if the
+  // range is inclusive). If the range is moving backwards, it's
+  // finished if the current value is *less than* or equal to the end
+  // point (just less than if the range is inclusive).
+  const finished = () => fwd ? i ? cur > e : cur >= e : i ? cur < e : cur <= e
 
-  // `current` is a local variable not readable from outside this
-  // function, so there's no need to worry about it being updated
-  // during the yield
+  // `cur` is a local variable not readable from outside this function,
+  // so there's no need to worry about it being updated during the yield
   /* eslint-disable require-atomic-updates */
   while (!finished()) {
-    yield current
-    current = forward ? current + p : current - p
+    yield cur
+    cur = fwd ? cur + p : cur - p
   }
   /* eslint-enable require-atomic-updates */
 }
---
+++ test/iterators.js
@@ -0,0 +1,121 @@
+// Copyright (c) 2020 Thomas J. Otterson
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { expect } from 'chai'
+
+import { enumerate, range } from 'modules/iterators'
+
+describe('Iterator functions', () => {
+  describe('enumerate', () => {
+    it('yields a series of values along with their indexes', () => {
+      const result = [...enumerate(['a', 'b', 'c', 'd', 'e'])]
+      expect(result).to.deep.equal([
+        [0, 'a'], [1, 'b'], [2, 'c'], [3, 'd'], [4, 'e'],
+      ])
+    })
+    it('works with strings', () => {
+      const result = [...enumerate('abcde')]
+      expect(result).to.deep.equal([
+        [0, 'a'], [1, 'b'], [2, 'c'], [3, 'd'], [4, 'e'],
+      ])
+    })
+    it('works with iterators', () => {
+      function *test() {
+        for (const letter of 'abcde') yield letter
+      }
+      const result = [...enumerate(test())]
+      expect(result).to.deep.equal([
+        [0, 'a'], [1, 'b'], [2, 'c'], [3, 'd'], [4, 'e'],
+      ])
+    })
+    it('can start indexes at a point other than 0', () => {
+      const result = [...enumerate('abcde', 4)]
+      expect(result).to.deep.equal([
+        [4, 'a'], [5, 'b'], [6, 'c'], [7, 'd'], [8, 'e'],
+      ])
+    })
+    it('will drop the fractional part off floating point start indexes', () => {
+      const result = [...enumerate('abcde', 4.6)]
+      expect(result).to.deep.equal([
+        [4, 'a'], [5, 'b'], [6, 'c'], [7, 'd'], [8, 'e'],
+      ])
+    })
+    it('allows negative start indexes', () => {
+      const result = [...enumerate('abcde', -4.6)]
+      expect(result).to.deep.equal([
+        [-4, 'a'], [-3, 'b'], [-2, 'c'], [-1, 'd'], [0, 'e'],
+      ])
+    })
+  })
+
+  describe('range', () => {
+    it('produces an exclusive range using an end point', () => {
+      const result = [...range(10)]
+      expect(result).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+    it('produces an exclusive range using start and end points', () => {
+      const result = [...range(1, 10)]
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+    it('produces an exclusive range using start, end, and step', () => {
+      const result = [...range(1, 11, 2)]
+      expect(result).to.deep.equal([1, 3, 5, 7, 9])
+    })
+    it('produces an inclusive range using an end point', () => {
+      const result = [...range(10, true)]
+      expect(result).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+    it('produces an inclusive range using start and end points', () => {
+      const result = [...range(1, 10, true)]
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+    it('produces an inclusive range using start, end, and step', () => {
+      const result = [...range(1, 11, 2, true)]
+      expect(result).to.deep.equal([1, 3, 5, 7, 9, 11])
+    })
+    it('produces exclusive reverse ranges using an end point', () => {
+      const result = [...range(-5)]
+      expect(result).to.deep.equal([0, -1, -2, -3, -4])
+    })
+    it('produces exclusive reverse ranges using start and end points', () => {
+      const result = [...range(10, 0)]
+      expect(result).to.deep.equal([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    })
+    it('produces exclusive reverse ranges using start, end, and step', () => {
+      const result = [...range(10, 0, 2)]
+      expect(result).to.deep.equal([10, 8, 6, 4, 2])
+    })
+    it('produces inclusive reverse ranges using an end point', () => {
+      const result = [...range(-5, true)]
+      expect(result).to.deep.equal([0, -1, -2, -3, -4, -5])
+    })
+    it('produces inclusive reverse ranges using start and end points', () => {
+      const result = [...range(10, 0, true)]
+      expect(result).to.deep.equal([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+    })
+    it('produces exclusive reverse ranges using start, end, and step', () => {
+      const result = [...range(10, 0, 2, true)]
+      expect(result).to.deep.equal([10, 8, 6, 4, 2, 0])
+    })
+    it('is equal in inclusive/exclusive if end is not part of range', () => {
+      const exclusive = [...range(1, 10, 2)]
+      expect(exclusive).to.deep.equal([1, 3, 5, 7, 9])
+      const inclusive = [...range(1, 10, 2, true)]
+      expect(inclusive).to.deep.equal([1, 3, 5, 7, 9])
+    })
+    it('can use floating point numbers', () => {
+      const result = [...range(0.5, 2.5, 0.5)]
+      expect(result).to.deep.equal([0.5, 1.0, 1.5, 2.0])
+    })
+    it('rejects steps of 0 and regards them as 1 instead', () => {
+      const result = [...range(1, 10, 0)]
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+    it('rejects negative steps and regards them as positive instead', () => {
+      const result = [...range(1, 10, -1)]
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+  })
+})
